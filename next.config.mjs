45ceaa73c62d1912419bb3faa892f8921bb97a56
Ripple-Tech <ifeanyiinnocentokoro@gmail.com>
2024-11-05 02:35:22 +0100

    eslint

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
         domains: ['drive.google.com', "" ], 
       },
+      eslint: {
+        dirs: ['utils', ], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+      },
 };
 
 export default nextConfig;
